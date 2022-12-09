06f099c795ec77e749acc7dd8d24f3397053c14a
Martin Freiberger <martin.freiberger@zeiss.com>
2022-12-09 14:22:58 +0100

    added dist folder

---
+++ dist/spa/assets/QSpace.d2f34c7f.js
@@ -0,0 +1,1 @@
+import{c as e}from"./use-router-link.0c6ea2e7.js";import{h as a}from"./index.cceb1773.js";const p=a("div",{class:"q-space"});var o=e({name:"QSpace",setup(){return()=>p}});export{o as Q};
